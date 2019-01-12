cade6619c218c419567238ef7753f8fe9e1b17d7
MaximeD <staphir@gmail.com>
2019-01-12 18:41:40 +0100

    Begin commit

---
+++ app/src/main/java/dropgame/devreese/dulieu/com/dropgame/MainActivity.java
@@ -0,0 +1,13 @@
+package dropgame.devreese.dulieu.com.dropgame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
